dfc9383a5543ad1db1ca511c1fc79e50ce17a19f
Sylvanus <sepehr.haghdoust@gmail.com>
2024-04-02 23:32:44 +0330

    image rendering in native

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,20 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 import EditScreenInfo from "@/src/components/EditScreenInfo";
 import Colors from "@/src/constants/Colors";
 import products from "@/assets/data/products";
+
+const product = products[1];
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Pizza Peperoni</Text>
-      <Text style={styles.price}>$12.99</Text>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-  
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
   },
 
   separator: {
@@ -22,9 +28,13 @@ const styles = StyleSheet.create({
     height: 1,
     width: "80%",
   },
-  title: { fontSize: 18, fontWeight: "600" , marginVertical: 10},
+  title: { fontSize: 18, fontWeight: "600", marginVertical: 10 },
   price: {
-color: Colors.light.tint,
-fontWeight: 'bold'
-  }
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
 });
